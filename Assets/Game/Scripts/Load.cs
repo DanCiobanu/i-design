a933ea4bde4a8ddc314ca02be7083f8d92f6b627
Dan Ciobanu <danutsciobanu@gmail.com>
2018-03-30 00:52:31 +0300

    added menu basic functionality

--- Assets/Game/Scripts/Load.cs
+++ Assets/Game/Scripts/Load.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Menu.Scripts.GameScripts
 {
-	public class Load : MonoBehaviour {
-
+	public class Load : MonoBehaviour
+	{
+		private int sceneIndex;
+		
 		// Use this for initialization
 		void Start ()
 		{
 			Screen.orientation = ScreenOrientation.Landscape;
+			sceneIndex = SceneManager.GetActiveScene().buildIndex;
 		}
 	
 		// Update is called once per frame
 		void Update () {
-		
+			if (Application.platform == RuntimePlatform.Android)
+			{
+				if (Input.GetKey(KeyCode.Escape))
+				{
+					SceneManager.LoadScene(sceneIndex - 1);
+				}
+			}
 		}
 	}
 }
